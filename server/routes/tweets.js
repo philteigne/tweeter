e759b277673340ff16a2ee26e1b567cac72045cd
Phil <philippe.teigne@gmail.com>
2023-11-28 10:33:11 -0500

    sync server

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper    = require("../lib/util/user-helper");
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
@@ -18,6 +18,7 @@ module.exports = function(DataHelpers) {
   });
 
   tweetsRoutes.post("/", function(req, res) {
+    console.log(`received data: ${req.body}`);
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
@@ -43,4 +44,4 @@ module.exports = function(DataHelpers) {
 
   return tweetsRoutes;
 
-}
+};
