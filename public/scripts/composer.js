fdc558aa4a41312c145d726405e36b85d8398605
Phil <philippe.teigne@gmail.com>
2023-12-01 16:48:57 -0500

    add scroll event listener to document that reveals and hides nav and return to top, add click listener on return to top

---
+++ public/scripts/composer.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $(document).scroll(() => {
+    if ($(this).scrollTop() === 0) {
+      $('.top-return').css("display", "none");
+      $('nav').slideDown();
+      return;
+    }
+    
+    $('.top-return').css("display", "flex");
+    $('nav').slideUp();
+
+  });
+
+
+  $('.top-return').on('click', () => {
+    window.scrollTo(0, 0);
+    if ($('.write-tweet').css('display') === "none") {
+      $('.write-tweet').slideDown();
+      return;
+    }
+  });
+
+});
