2860beb49bd523028334175f6372cbc0006a4dfc
Phil <philippe.teigne@gmail.com>
2023-12-05 23:18:41 -0500

    add red color to negative character counter amounts

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 
   $("#tweet-text").on('input', function() {
     let wordCount = $(this).val().length;
+    console.log("wordCount", wordCount);
+    if (wordCount > 140) {
+      $('output').css("color", "red");
+    }
+    if (wordCount < 140) {
+      $('output').css("color", "black");
+    }
+    
     console.log($(this.parentNode).find('.counter').val(140 - wordCount));
   });
 });
