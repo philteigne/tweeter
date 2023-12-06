9f6be9137a9346d3b33eb22ea0b750b6df61aed6
Phil <philippe.teigne@gmail.com>
2023-12-05 23:29:14 -0500

    reformat and add comments

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
   $(document).scroll(() => {
+    //  hide top-return if user is at the top of the page
     if ($(this).scrollTop() === 0) {
       $('.top-return').css("display", "none");
       $('nav').slideDown();
       return;
     }
     
+    //  show the top return and hide nav bar if the user scrolls down
     $('.top-return').css("display", "flex");
     $('nav').slideUp();
 
   });
 
+  //  top return click listener
   $('.top-return').on('click', () => {
     window.scrollTo(0, 0);
     if ($('.write-tweet').css('display') === "none") {
